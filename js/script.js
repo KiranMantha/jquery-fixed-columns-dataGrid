74466c4b9019b6e0b7dd6431937f08f5e943ea4b
Kiran Mantha <kiranv.mantha@gmail.com>
2017-09-20 19:30:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $('#example').dataGrid({
         left: 2,
         containerId: 'tblContainer'
+        'z-index': 4,
+        containerHeight: '620px',
+        containerWidth: '500px'
     });
     //$('#example').dataGrid.destroy();
 });
